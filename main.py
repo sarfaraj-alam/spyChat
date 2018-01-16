398e465357d186887f7d5a5335b22358dafec70d
sarfaraj-alam <sarfaraj.lpu@gmail.com>
2018-01-16 20:23:39 +0530

    Just added comments

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 print "Let's Start"
+# taking input for Spy Name
 spyName = input("Enter Spy Name : ")
+# Ask for Initial of Spy Name
 spySalutation = input("What should we call you (Mr. or Ms.)?")
+# concatenation of initial and spy name
 spyName = spySalutation + " " + spyName
+# printing the name of Spy with predefined message.
 print "Alrigh " +spyName+ ". " + "I'd like to know a little bit more about..."
