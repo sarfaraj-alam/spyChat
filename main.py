3dbe042bbcdc73c0dce13614f2fc86e7b0b51f38
sarfaraj-alam <sarfaraj.lpu@gmail.com>
2018-01-16 20:19:45 +0530

    Change raw_input() to input()

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 print "Let's Start"
-spyName = raw_input("Enter Spy Name : ")
-spySalutation = raw_input("What should we call you (Mr. or Ms.)?")
+spyName = input("Enter Spy Name : ")
+spySalutation = input("What should we call you (Mr. or Ms.)?")
 spyName = spySalutation + " " + spyName
 print "Alrigh " +spyName+ ". " + "I'd like to know a little bit more about..."
